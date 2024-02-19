e27414e46795947ca39563059fffebba26a13a1b
Nat <nathaliasalinas97@gmail.com>
2024-02-18 21:47:53 -0500

    added PySimpleGUI library. Slider to determine password length and displays generated password in a textbox.

--- password_generator.py
+++ password_generator.py
@@ -1,19 +1,42 @@
 import random 
-
-print('Welcome to your password generator')
+import PySimpleGUI as pg 
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(),'
 
-number = input('Amount of passwords to generate: ')
-number = int(number)
-
-length = input('Inpuut your password length: ')
-length = int(length)
-
-print('\nhere are your passwords: ')
-
-for pwd in range(number):
+def generate_password(length):
     passwords = ''
     for c in range(length):
         passwords += random.choice(chars)
-    print(passwords)
+    
+    return passwords
+
+# Set a theme
+pg.theme("BluePurple")
+
+# Display generated password in Text box
+output = pg.Text()
+
+# Create Layout 
+layout = [
+    [output],
+    [pg.Slider(range=(8,32), default_value=12, expand_x=True, enable_events=True, orientation='horizontal')],
+    [pg.Button("Generate"), pg.Button("Cancel")]
+]
+
+# Create Window 
+window =  pg.Window("Password Generator", layout)
+
+# Event Loop 
+while True:
+    event, values = window.read()
+    print(event, values)
+    new_password = generate_password(int(values[0]))
+    output.update(value=new_password)
+    if event == "Cancel" or event == pg.WIN_CLOSED:
+        break 
+
+# Close Window 
+window.close()
+
+
+ 
